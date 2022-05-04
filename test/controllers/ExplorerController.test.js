39d3223d03513513001d65a87394bf23f3417e94
AleRamRey <launchx09432@innovaccion.mx>
2022-05-03 19:10:53 -0500

    Se corre revision de codigo linter

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,20 +1,20 @@
-const ExplorerController = require("./../../lib/controllers/ExplorerController")
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Test for ExplorerController", () => {
 
     test("Filtrar lista de explorers por mision ", () => {
-        const explorers = ExplorerController.getExplorersByMission("node")
-        expect(explorers.length).toBe(10)
+        const explorers = ExplorerController.getExplorersByMission("node");
+        expect(explorers.length).toBe(10);
     }),
 
     test("Obtener explorer por nombre y mision ", () => {
-        const explorers = ExplorerController.getExplorersUsernamesByMission("node")
-        expect(explorers[0]).toBe("ajolonauta1")
+        const explorers = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorers[0]).toBe("ajolonauta1");
     }),
 
     test("Obtener explorer por nombre y mision ", () => {
-        const explorers = ExplorerController.getExplorersAmonutByMission("java")
-        expect(explorers).toBe(5)
-    })
+        const explorers = ExplorerController.getExplorersAmonutByMission("java");
+        expect(explorers).toBe(5);
+    });
 
 });
